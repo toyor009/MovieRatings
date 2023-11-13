4fb13cbe1b7f917d3689c6dd4d5fc65df6023e5f
Toyosi Olayiwola <omotoyosiolayiwola@gmail.com>
2023-11-13 13:09:46 +0100

    extract movie item component

--- src/App.vue
+++ src/App.vue
@@ -24,90 +24,14 @@
     </div>
 
     <div class="lg:grid grid-cols-3 gap-4 my-4">
-      <div
+      <MovieItem
         v-for="(movie, movieIndex) in movies"
-        class="mb-4 lg:mb-0"
-        :key="movieIndex"
-        @mouseenter="activeMovieIndex = String(movieIndex)"
-        @mouseleave="activeMovieIndex = ''"
-      >
-        <div class="relative">
-          <!-- Star -->
-          <StarIcon class="absolute right-1 w-11 h-11 text-yellow-500" />
-          <h1 class="absolute top-3 right-[23px] text-red-950">
-            {{ movie.rating || "-" }}
-          </h1>
-        </div>
-
-        <!-- Image -->
-        <img :src="movie.image" alt="image" class="w-full h-[520px]" />
-
-        <div class="min-h-[260px] bg-white p-4 rounded-b flex flex-col">
-          <div class="flex-1">
-            <!-- Title -->
-            <h1 class="font-bold text-xl">{{ movie.name }}</h1>
-
-            <!-- Genres -->
-            <div class="mt-2 flex gap-1">
-              <div
-                v-for="(genre, genIndex) in movie.genres"
-                :key="genIndex"
-                class="px-2 bg-[#6566F0] rounded-xl text-white text-sm font-semibold"
-              >
-                {{ genre }}
-              </div>
-            </div>
-
-            <!-- Description -->
-            <p class="mt-3 font-semibold">
-              {{ movie.description }}
-            </p>
-          </div>
-
-          <div class="mt-5 flex items-center justify-between font-semibold">
-            <!-- Rating -->
-            <div class="flex items-center gap-2">
-              Rating: ({{ movie.rating }}/5)
-              <div class="flex items-center">
-                <button
-                  v-for="(star, starIndex) in 5"
-                  :key="starIndex"
-                  @click="updateRating(movieIndex, star)"
-                >
-                  <StarIcon
-                    class="w-[21px] h-[19px]"
-                    :class="
-                      movie.rating >= star ? 'text-yellow-500' : 'text-gray-500'
-                    "
-                  />
-                </button>
-              </div>
-            </div>
-
-            <!-- Actions -->
-            <div
-              v-show="activeMovieIndex == String(movieIndex)"
-              class="flex items-center gap-3"
-            >
-              <!-- Edit -->
-              <span title="Edit movie">
-                <PencilIcon
-                  class="w-[21px] h-[19px] cursor-pointer hover:text-blue-500"
-                  @click="startMovieEdit(movieIndex, movie)"
-                />
-              </span>
-
-              <!-- Delete -->
-              <span title="Delete movie">
-                <TrashIcon
-                  class="w-[21px] h-[19px] cursor-pointer hover:text-red-500"
-                  @click="movies.splice(movieIndex, 1)"
-                />
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+        :key="`${movieIndex}-${movie.name}`"
+        :movie="movie"
+        @edit="startMovieEdit(movieIndex, movie)"
+        @remove="movies.splice(movieIndex, 1)"
+        @update:rating="updateRating(movieIndex, $event)"
+      />
     </div>
   </div>
 
@@ -126,13 +50,13 @@
 import { computed, ref } from "vue";
 
 import { movieItems } from "./movies.json";
-import { StarIcon, PencilIcon, TrashIcon } from "@heroicons/vue/24/solid";
 
 import Overlay from "./components/Overlay.vue";
 import AddNewMovieForm from "./components/AddNewMovieForm.vue";
+import MovieItem from "./components/MovieItem.vue";
 
 const movies = ref([...movieItems]);
-const activeMovieIndex = ref("");
+
 const editingMovieIndex = ref(null);
 const editingMovie = ref({});
 const showAddMovieModal = ref(false);
@@ -161,7 +85,7 @@ function updateMovie(movie) {
 
 function startMovieEdit(movieIndex, movie) {
   editingMovieIndex.value = movieIndex;
-  editingMovie.value = movie;
+  editingMovie.value = { ...movie };
   showAddMovieModal.value = true;
 }
 
---
+++ src/components/MovieItem.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    class="mb-4 lg:mb-0"
+    @mouseenter="activeMovieIndex = String(movie.id)"
+    @mouseleave="activeMovieIndex = ''"
+  >
+    <div class="relative">
+      <!-- Star -->
+      <StarIcon class="absolute right-1 w-11 h-11 text-yellow-500" />
+      <h1 class="absolute top-3 right-[23px] text-red-950">
+        {{ movie.rating || "-" }}
+      </h1>
+    </div>
+
+    <!-- Image -->
+    <img :src="movie.image" alt="image" class="w-full h-[520px]" />
+
+    <div class="min-h-[260px] bg-white p-4 rounded-b flex flex-col">
+      <div class="flex-1">
+        <!-- Title -->
+        <h1 class="font-bold text-xl">{{ movie.name }}</h1>
+
+        <!-- Genres -->
+        <div class="mt-2 flex gap-1">
+          <div
+            v-for="(genre, genIndex) in movie.genres"
+            :key="genIndex"
+            class="px-2 bg-[#6566F0] rounded-xl text-white text-sm font-semibold"
+          >
+            {{ genre }}
+          </div>
+        </div>
+
+        <!-- Description -->
+        <p class="mt-3 font-semibold">
+          {{ movie.description }}
+        </p>
+      </div>
+
+      <div class="mt-5 flex items-center justify-between font-semibold">
+        <!-- Rating -->
+        <div class="flex items-center gap-2">
+          Rating: ({{ movie.rating }}/5)
+          <div class="flex items-center">
+            <button
+              v-for="(star, starIndex) in 5"
+              :key="starIndex"
+              @click="emit('update:rating', star)"
+            >
+              <StarIcon
+                class="w-[21px] h-[19px]"
+                :class="
+                  movie.rating >= star ? 'text-yellow-500' : 'text-gray-500'
+                "
+              />
+            </button>
+          </div>
+        </div>
+
+        <!-- Actions -->
+        <div
+          v-show="activeMovieIndex == String(movie.id)"
+          class="flex items-center gap-3"
+        >
+          <!-- Edit -->
+          <span title="Edit movie">
+            <PencilIcon
+              class="w-[21px] h-[19px] cursor-pointer hover:text-blue-500"
+              @click="emit('edit')"
+            />
+          </span>
+
+          <!-- Delete -->
+          <span title="Delete movie">
+            <TrashIcon
+              class="w-[21px] h-[19px] cursor-pointer hover:text-red-500"
+              @click="emit('remove')"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, PropType } from "vue";
+
+import { StarIcon, PencilIcon, TrashIcon } from "@heroicons/vue/24/solid";
+
+import { Movie } from "../types";
+
+defineOptions({
+  name: "movie-item",
+});
+
+const props = defineProps<{
+  movie: Movie;
+}>();
+
+const emit = defineEmits(["edit", "remove", "update:rating"]);
+
+const activeMovieIndex = ref("");
+</script>
